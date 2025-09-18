8cbc73113a7b905a5f765554c8eed923d5c21ecb
chaidanSnru <chaidan@snru.ac.th>
2025-09-18 15:48:11 +0700

    แก้ปัญหา Constants.kt สร้างผิดที่ไปสร้างที่ app ที่ถูกต้องสร้างที่ package ปรับถึง FirstFragment.kt

---
+++ app/src/main/java/com/example/androidfragment/Constants.kt
@@ -0,0 +1,7 @@
+// file: Constants.kt
+package com.example.androidfragment
+
+// ประกาศเป็น Top-level constant
+// ไม่ต้องอยู่ในคลาสใดๆ ทำให้เรียกใช้จากที่ไหนก็ได้
+const val REQUEST_KEY = "my_request_key"
+const val BUNDLE_KEY = "my_bundle_key"
